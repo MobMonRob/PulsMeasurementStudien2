7d1f86ee57f642cd0571f11834f84cb9ba4d75ff
kck278 <kim-celine.kahl@web.de>
2019-11-26 13:31:42 +0100

    removed command line arguments

--- src/kontaktlose_pulsmessung/src/pulse_polarH7.py
+++ src/kontaktlose_pulsmessung/src/pulse_polarH7.py
@@ -38,32 +38,8 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description="Bluetooth heart rate monitor data logger")
     parser.add_argument("-m", metavar='MAC', type=str, help="MAC address of BLE device (default: auto-discovery)")
-    parser.add_argument("-b", action='store_true', help="Check battery level")
     parser.add_argument("-g", metavar='PATH', type=str, help="gatttool path (default: system available)", default="gatttool")
-    parser.add_argument("-o", metavar='FILE', type=str, help="Output filename of the database (default: none)")
     parser.add_argument("-H", metavar='HR_HANDLE', type=str, help="Gatttool handle used for HR notifications (default: none)")
-    parser.add_argument("-v", action='store_true', help="Verbose output")
-    parser.add_argument("-d", action='store_true', help="Enable debug of gatttool")
-
-    confpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "BLEHeartRateLogger.conf")
-    if os.path.exists(confpath):
-
-        config = configparser.SafeConfigParser()
-        config.read([confpath])
-        config = dict(config.items("config"))
-
-        # We compare here the configuration given in the config file with the
-        # configuration of the parser.
-        args = vars(parser.parse_args([]))
-        err = False
-        for key in config.iterkeys():
-            if key not in args:
-                log.error("Configuration file error: invalid key '" + key + "'.")
-                err = True
-        if err:
-            sys.exit(1)
-
-        parser.set_defaults(**config)
 
     return parser.parse_args()
 
@@ -346,13 +322,8 @@ def cli():
         log.critical("Couldn't find gatttool path!")
         sys.exit(1)
 
-    # Increase verbose level
-    if args.v:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
     try:
-        main(args.m, args.o, args.g, args.b, args.H, args.d)
+        main(addr=args.m, gatttool=args.g, hr_handle=args.H)
     except rospy.ROSInterruptException:
         pass
 

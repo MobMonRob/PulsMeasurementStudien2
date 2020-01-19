5f58e60d0f1796b71be039aaf9a4eb9d4abb927a
yakuhzi <miguel@heidegger-ueberlingen.de>
2020-01-19 18:10:19 +0100

    implemented biggest face detection

--- src/face_detection/src/main.py
+++ src/face_detection/src/main.py
@@ -38,23 +38,47 @@ class ImageConverter:
 
         if len(faces) is 0:
             rospy.loginfo("No faces detected!")
+            cv2.imshow("Image", cv_image)
+            cv2.waitKey(3)
             return
 
-        face = faces[0]
-        x = face[0]
-        y = face[1]
-        w = face[2]
-        h = face[3]
+        biggest_face = self.get_biggest_face(faces)
+        face_x = biggest_face[0]
+        face_y = biggest_face[1]
+        face_w = biggest_face[2]
+        face_h = biggest_face[3]
+        cv2.rectangle(cv_image, (face_x, face_y), (face_x + face_w, face_y + face_h), (255, 0, 0), 2)
 
-        cropped_image = cv_image[y: y + (int(h / 3.2)), x + (w / 4): x + (w / 4) + (w / 2)]
+        forehead_x = face_x + face_w / 4
+        forehead_y = face_y
+        forehead_w = face_w / 2
+        forehead_h = int(face_h / 3.2)
 
-        cv2.imshow("Cropped", cropped_image)
+        cropped_image = cv_image[forehead_y: forehead_y + forehead_h, forehead_x: forehead_x + forehead_w]
+        cv2.rectangle(
+            cv_image,
+            (forehead_x, forehead_y),
+            (forehead_x + forehead_w, forehead_y + forehead_h),
+            (0, 0, 255),
+            2
+        )
+
+        cv2.imshow("Image", cv_image)
         cv2.waitKey(3)
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cropped_image, "bgr8"))
         except CvBridgeError as e:
-            print(e)
+            print(e).0
+
+    def get_biggest_face(self, faces):
+        biggest_face = None
+
+        for face in faces:
+            if biggest_face is None or face[2] > biggest_face[2] and face[3] > biggest_face[3]:
+                biggest_face = face
+
+        return biggest_face
 
 
 def main(args):

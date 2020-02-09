26bbe37c5b234832fcdd7b87b7f881464d0061ff
yakuhzi <miguel@heidegger-ueberlingen.de>
2020-02-09 14:21:56 +0100

    Fix face crop and added comments

--- src/face_detection/src/main.py
+++ src/face_detection/src/main.py
@@ -19,49 +19,62 @@ class ImageConverter:
 
     def callback(self, data):
         try:
+            # Convert ROS image to OpenCV image
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
             return
 
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        # Get gray scale image from OpenCV
+        gray_scale_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
         # Create the haar cascade
         face_cascade = cv2.CascadeClassifier(os.path.dirname(os.path.realpath(__file__)) + "/../resources/cascade.xml")
 
         # Detect faces in the image
         faces = face_cascade.detectMultiScale(
-            gray,
+            gray_scale_image,
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30)
         )
 
+        # Show original image, if no faces are detected
         if len(faces) is 0:
             rospy.loginfo("No faces detected!")
             cv2.imshow("Image", cv_image)
             cv2.waitKey(3)
             return
 
+        # Get biggest face
         biggest_face = self.get_biggest_face(faces)
         face_x = biggest_face[0]
         face_y = biggest_face[1]
         face_w = biggest_face[2]
         face_h = biggest_face[3]
+
+        # Crop image to biggest face
+        cropped_face = cv_image[face_y: face_y + face_h, face_x: face_x + face_w]
+        # Visualize face in original image
         cv2.rectangle(cv_image, (face_x, face_y), (face_x + face_w, face_y + face_h), (255, 0, 0), 2)
 
         try:
-            ros_img = self.bridge.cv2_to_imgmsg(cropped_image, "bgr8")
+            # Convert OpenCV image back to ROS image
+            ros_img = self.bridge.cv2_to_imgmsg(cropped_face, "bgr8")
+            # Publish image to ROS-Topic
             self.face_publisher.publish(ros_img)
         except CvBridgeError as e:
             print(e).0
 
+        # Define region of forhead
         forehead_x = face_x + face_w / 4
         forehead_y = face_y
         forehead_w = face_w / 2
         forehead_h = int(face_h / 3.2)
 
-        cropped_image = cv_image[forehead_y: forehead_y + forehead_h, forehead_x: forehead_x + forehead_w]
+        # Crop image to forehead
+        cropped_forehead = cv_image[forehead_y: forehead_y + forehead_h, forehead_x: forehead_x + forehead_w]
+        # Visualize forehead in original image
         cv2.rectangle(
             cv_image,
             (forehead_x, forehead_y),
@@ -71,11 +84,14 @@ class ImageConverter:
         )
 
         try:
-            ros_img = self.bridge.cv2_to_imgmsg(cropped_image, "bgr8")
+            # Convert OpenCV image back to ROS image
+            ros_img = self.bridge.cv2_to_imgmsg(cropped_forehead, "bgr8")
+            # Publish image to ROS-Topic
             self.forehead_publisher.publish(ros_img)
         except CvBridgeError as e:
             print(e).0
 
+        # Show original image with visualized face and forehead
         cv2.imshow("Image", cv_image)
         cv2.waitKey(3)
 
@@ -83,6 +99,7 @@ class ImageConverter:
         biggest_face = None
 
         for face in faces:
+            # If width and height of rectangle is bigger, set biggest_face to current face
             if biggest_face is None or face[2] > biggest_face[2] and face[3] > biggest_face[3]:
                 biggest_face = face
 
@@ -92,10 +109,12 @@ class ImageConverter:
 def main(args):
     rospy.init_node('face_detection', anonymous=True, log_level=rospy.DEBUG)
 
+    # Get ROS topic from launch parameter
     # topic = rospy.get_param("~topic", "/pylon_camera_node/image_raw")
     topic = rospy.get_param("~topic", "/webcam/image_raw")
     rospy.loginfo("Listening on topic '" + topic + "'")
 
+    # Start image converter
     image_converter = ImageConverter(topic)
 
     try:

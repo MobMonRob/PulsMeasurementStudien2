f0b99e8f12c7a5c6cc05a81c3f42bb49f7ff06e4
yakuhzi <miguel@heidegger-ueberlingen.de>
2020-02-27 06:30:39 -0800

    Remove face detection parts

--- src/webcam_pulse_detector/src/main.py
+++ src/webcam_pulse_detector/src/main.py
@@ -11,36 +11,19 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
 from interface import plotXY, imshow, waitKey, destroyWindow
-from processors import FindFaceGetPulse
+from processors import GetPulse
 
 
 class PulseApp(object):
-    """
-    Python application that finds a face in a webcam stream, then isolates the
-    forehead.
-
-    Then the average green-light intensity in the forehead region is gathered
-    over time, and the detected person's pulse is estimated.
-    """
-
     def __init__(self, topic):
         self.image_sub = rospy.Subscriber(topic, Image, self.callback)
         self.bridge = CvBridge()
 
         self.w, self.h = 0, 0
         self.pressed = 0
-        # Containerized analysis of recieved image frames (an openMDAO assembly)
-        # is defined next.
 
-        # This assembly is designed to handle all image & signal analysis,
-        # such as face detection, forehead isolation, time series collection,
-        # heart-beat detection, etc.
-
-        # Basically, everything that isn't communication
-        # to the camera device or part of the GUI
-        self.processor = FindFaceGetPulse(bpm_limits=[50, 160],
-                                          data_spike_limit=2500.,
-                                          face_detector_smoothness=10.)
+        # Basically, everything that isn't part of the GUI
+        self.processor = GetPulse()
 
         # Init parameters for the cardiac data plot
         self.bpm_plot = False
@@ -48,51 +31,27 @@ class PulseApp(object):
 
         # Maps keystrokes to specified methods
         # (A GUI window must have focus for these to work)
-        self.key_controls = {"s": self.toggle_search,
-                             "d": self.toggle_display_plot,
-                             "f": self.write_csv}
+        self.key_controls = {"d": self.toggle_display_plot, "f": self.write_csv}
 
     def write_csv(self):
-        """
-        Writes current data to a csv file
-        """
         fn = "Webcam-pulse" + str(datetime.datetime.now())
         fn = fn.replace(":", "_").replace(".", "_")
         data = np.vstack((self.processor.times, self.processor.samples)).T
         np.savetxt(fn + ".csv", data, delimiter=',')
         print("Writing csv")
 
-    def toggle_search(self):
-        """
-        Toggles a motion lock on the processor's face detection component.
-
-        Locking the forehead location in place significantly improves
-        data quality, once a forehead has been sucessfully isolated.
-        """
-        # state = self.processor.find_faces.toggle()
-        state = self.processor.find_faces_toggle()
-        print("face detection lock =", not state)
-
     def toggle_display_plot(self):
-        """
-        Toggles the data display.
-        """
         if self.bpm_plot:
             print("bpm plot disabled")
             self.bpm_plot = False
             destroyWindow(self.plot_title)
         else:
             print("bpm plot enabled")
-            if self.processor.find_faces:
-                self.toggle_search()
             self.bpm_plot = True
             self.make_bpm_plot()
             moveWindow(self.plot_title, self.w, 0)
 
     def make_bpm_plot(self):
-        """
-        Creates and/or updates the data display
-        """
         plotXY([[self.processor.times,
                  self.processor.samples],
                 [self.processor.freqs,
@@ -106,13 +65,6 @@ class PulseApp(object):
                bg=self.processor.slices[0])
 
     def key_handler(self):
-        """
-        Handle keystrokes, as set at the bottom of __init__()
-
-        A plotting or camera frame window must have focus for keypresses to be
-        detected.
-        """
-
         self.pressed = waitKey(10) & 255  # wait for keypress for 10 ms
         if self.pressed == 27:  # exit program on 'esc'
             print("Exiting")
@@ -143,7 +95,7 @@ class PulseApp(object):
         output_frame = self.processor.frame_out
 
         # show the processed/annotated output frame
-        imshow("Processed", output_frame)
+        # imshow("Processed", output_frame)
 
         # create and/or update the raw data display if needed
         if self.bpm_plot:
@@ -160,7 +112,7 @@ def main(args):
     topic = rospy.get_param("~topic", "/webcam/image_raw")
     rospy.loginfo("Listening on topic '" + topic + "'")
 
-    App = PulseApp(topic)
+    PulseApp(topic)
 
     try:
         rospy.spin()
--- src/webcam_pulse_detector/src/processors.py
+++ src/webcam_pulse_detector/src/processors.py
@@ -3,30 +3,15 @@ import time
 import cv2
 import pylab
 import os
-import sys
 
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath("../lib")
+class GetPulse(object):
 
-    return os.path.join(base_path, relative_path)
-
-
-class FindFaceGetPulse(object):
-
-    def __init__(self, bpm_limits=[], data_spike_limit=250,
-                 face_detector_smoothness=10):
-
+    def __init__(self):
         self.frame_in = np.zeros((10, 10))
         self.frame_out = np.zeros((10, 10))
         self.fps = 0
         self.buffer_size = 250
-        # self.window = np.hamming(self.buffer_size)
         self.data_buffer = []
         self.times = []
         self.ttimes = []
@@ -40,53 +25,19 @@ class FindFaceGetPulse(object):
         self.face_cascade = cv2.CascadeClassifier(
             os.path.dirname(os.path.realpath(__file__)) + "/../resources/cascade.xml")
 
-        self.face_rect = [1, 1, 2, 2]
         self.last_center = np.array([0, 0])
         self.last_wh = np.array([0, 0])
         self.output_dim = 13
-        self.trained = False
 
         self.idx = 1
         self.find_faces = True
 
-    def find_faces_toggle(self):
-        self.find_faces = not self.find_faces
-        return self.find_faces
-
-    def get_faces(self):
-        return
-
-    def shift(self, detected):
-        x, y, w, h = detected
-        center = np.array([x + 0.5 * w, y + 0.5 * h])
-        shift = np.linalg.norm(center - self.last_center)
-
-        self.last_center = center
-        return shift
-
-    def draw_rect(self, rect, col=(0, 255, 0)):
-        x, y, w, h = rect
-        cv2.rectangle(self.frame_out, (x, y), (x + w, y + h), col, 1)
-
-    def get_subface_coord(self, fh_x, fh_y, fh_w, fh_h):
-        x, y, w, h = self.face_rect
-        return [int(x + w * fh_x - (w * fh_w / 2.0)),
-                int(y + h * fh_y - (h * fh_h / 2.0)),
-                int(w * fh_w),
-                int(h * fh_h)]
-
-    def get_subface_means(self, coord):
-        x, y, w, h = coord
-        subframe = self.frame_in[y:y + h, x:x + w, :]
-        v1 = np.mean(subframe[:, :, 0])
-        v2 = np.mean(subframe[:, :, 1])
-        v3 = np.mean(subframe[:, :, 2])
+    def get_subface_means(self):
+        v1 = np.mean(self.frame_in[:, :, 0])
+        v2 = np.mean(self.frame_in[:, :, 1])
+        v3 = np.mean(self.frame_in[:, :, 2])
 
         return (v1 + v2 + v3) / 3.
-
-    def train(self):
-        self.trained = not self.trained
-        return self.trained
 
     def plot(self):
         data = np.array(self.data_buffer).T
@@ -116,52 +67,13 @@ class FindFaceGetPulse(object):
     def run(self):
         self.times.append(time.time() - self.t0)
         self.frame_out = self.frame_in
-        self.gray = cv2.equalizeHist(cv2.cvtColor(self.frame_in,
-                                                  cv2.COLOR_BGR2GRAY))
+        self.gray = cv2.equalizeHist(cv2.cvtColor(self.frame_in, cv2.COLOR_BGR2GRAY))
         col = (100, 255, 100)
-        if self.find_faces:
-            cv2.putText(
-                self.frame_out, "Press 'S' to lock face and begin",
-                (10, 25), cv2.FONT_HERSHEY_PLAIN, 1.25, col)
-            cv2.putText(self.frame_out, "Press 'Esc' to quit",
-                        (10, 50), cv2.FONT_HERSHEY_PLAIN, 1.25, col)
-            self.data_buffer, self.times, self.trained = [], [], False
-            detected = list(self.face_cascade.detectMultiScale(self.gray,
-                                                               scaleFactor=1.3,
-                                                               minNeighbors=4,
-                                                               minSize=(
-                                                                   50, 50),
-                                                               flags=cv2.CASCADE_SCALE_IMAGE))
+        # cv2.putText(self.frame_out, "Press 'S' to restart", (10, 25), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
+        # cv2.putText(self.frame_out, "Press 'D' to toggle data plot", (10, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
+        # cv2.putText(self.frame_out, "Press 'Esc' to quit", (10, 75), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
 
-            if len(detected) > 0:
-                detected.sort(key=lambda a: a[-1] * a[-2])
-
-                if self.shift(detected[-1]) > 10:
-                    self.face_rect = detected[-1]
-            forehead1 = self.get_subface_coord(0.5, 0.18, 0.25, 0.15)
-            self.draw_rect(self.face_rect, col=(255, 0, 0))
-            x, y, w, h = self.face_rect
-            cv2.putText(self.frame_out, "Face",
-                        (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
-            self.draw_rect(forehead1)
-            x, y, w, h = forehead1
-            cv2.putText(self.frame_out, "Forehead",
-                        (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
-            return
-        if set(self.face_rect) == set([1, 1, 2, 2]):
-            return
-        cv2.putText(
-            self.frame_out, "Press 'S' to restart",
-            (10, 25), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
-        cv2.putText(self.frame_out, "Press 'D' to toggle data plot",
-                    (10, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
-        cv2.putText(self.frame_out, "Press 'Esc' to quit",
-                    (10, 75), cv2.FONT_HERSHEY_PLAIN, 1.5, col)
-
-        forehead1 = self.get_subface_coord(0.5, 0.18, 0.25, 0.15)
-        self.draw_rect(forehead1)
-
-        vals = self.get_subface_means(forehead1)
+        vals = self.get_subface_means()
 
         self.data_buffer.append(vals)
         L = len(self.data_buffer)
@@ -196,33 +108,17 @@ class FindFaceGetPulse(object):
             self.fft = pruned
             idx2 = np.argmax(pruned)
 
-            t = (np.sin(phase[idx2]) + 1.) / 2.
-            t = 0.9 * t + 0.1
-            alpha = t
-            beta = 1 - t
-
             self.bpm = self.freqs[idx2]
             self.idx += 1
 
-            x, y, w, h = self.get_subface_coord(0.5, 0.18, 0.25, 0.15)
-            r = alpha * self.frame_in[y:y + h, x:x + w, 0]
-            g = alpha * \
-                self.frame_in[y:y + h, x:x + w, 1] + \
-                beta * self.gray[y:y + h, x:x + w]
-            b = alpha * self.frame_in[y:y + h, x:x + w, 2]
-            self.frame_out[y:y + h, x:x + w] = cv2.merge([r,
-                                                          g,
-                                                          b])
-            x1, y1, w1, h1 = self.face_rect
-            self.slices = [np.copy(self.frame_out[y1:y1 + h1, x1:x1 + w1, 1])]
-            col = (100, 255, 100)
+            self.frame_out = self.frame_in
+
             gap = (self.buffer_size - L) / self.fps
             # self.bpms.append(bpm)
             # self.ttimes.append(time.time())
             if gap:
-                text = "(estimate: %0.1f bpm, wait %0.0f s)" % (self.bpm, gap)
+                print("(Estimated: %0.1f bpm, wait %0.0f s)" % (self.bpm, gap))
             else:
-                text = "(estimate: %0.1f bpm)" % (self.bpm)
-            tsize = 1
-            cv2.putText(self.frame_out, text,
-                        (int(x - w / 2), int(y)), cv2.FONT_HERSHEY_PLAIN, tsize, col)
+                print("(Estimated: %0.1f bpm)" % self.bpm)
+
+            # cv2.putText(self.frame_out, text, (int(x - w / 2), int(y)), cv2.FONT_HERSHEY_PLAIN, tsize, col)

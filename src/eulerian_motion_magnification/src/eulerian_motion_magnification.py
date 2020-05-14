34ba6e04909e240f76daf911ce78c7eaa2a5f913
yakuhzi <miguel@heidegger-ueberlingen.de>
2020-05-14 17:58:42 +0200

    Use PulsePublischer in eulerian method, use parameter toggle show processed image

--- src/eulerian_motion_magnification/src/eulerian_motion_magnification.py
+++ src/eulerian_motion_magnification/src/eulerian_motion_magnification.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from __future__ import print_function
 import numpy as np
 import time
 import cv2
-import os
 import rospy
 import sys
 
-from std_msgs.msg import Float32, Float32MultiArray
 import scipy.fftpack as fftpack
-from scipy.signal import butter, filtfilt, find_peaks
-import matplotlib.pyplot as plt
-from face_detection import FaceDetector
+from scipy.signal import find_peaks
+from face_detector import FaceDetector
+from pulse_publisher import PulsePublisher
 
 
 def build_gaussian_pyramid(frame, level=3):
@@ -30,15 +27,15 @@ def build_gaussian_pyramid(frame, level=3):
     return pyramid
 
 
-def build_gaussian_frame(normalized, level):
+def build_gaussian_frame(normalized_frame, level):
     """
     Build gaussian pyramid. Level is indicating how deep the gaussian pyramid has to be.
     The actual logic to downsample is in method build_gaussian_pyramid.
     Only the last frame of the pyramid is needed.
-    :param level: how deep gaussian pyramid needs be calculated
     :param normalized_frame: frame to downsample
+    :param level: how deep gaussian pyramid needs be calculated
     """
-    pyramid = build_gaussian_pyramid(normalized, level)
+    pyramid = build_gaussian_pyramid(normalized_frame, level)
     gaussian_frame = pyramid[-1]
     return gaussian_frame
 
@@ -53,7 +50,6 @@ def temporal_bandpass_filter(video_to_filter, low, high, fps):
     """
     fft = fftpack.fft(video_to_filter, axis=0)
     frequencies = fftpack.fftfreq(video_to_filter.shape[0], d=1.0 / fps)
-    print("frequencies: " + str(frequencies))
     bound_low = (np.abs(frequencies - low)).argmin()
     bound_high = (np.abs(frequencies - high)).argmin()
     fft[:bound_low] = 0
@@ -75,13 +71,18 @@ def amplify_video(filtered_video, amplify):
     return amplification_array
 
 
-def calculate_pulse(processed_video, recorded_time):
+def calculate_pulse(processed_video, recorded_time, show_processed_image):
     """
     The processed images, saved in processed_video, is used as the data basis to calculate the pulse.
+    For the actual calculation, only the red values are needed. Therefore these are extracted with the method
+    extract_red_values.
     The mean value of all the pixels in an image is calculated and added to list red_values.
     On this array the peaks are detected and the amount of peaks is used to calculate bpm.
-    : param recorded_time: timespan, where images are collected in array
+    : param recorded_time: Timespan, where images are collected in array
     """
+    if show_processed_image:
+        processed_video = extract_red_values(processed_video, show_processed_image)
+        processed_video = np.asarray(processed_video, dtype=np.float32)
     red_values = []
     for i in range(0, processed_video.shape[0]):
         img = processed_video[i]
@@ -90,27 +91,72 @@ def calculate_pulse(processed_video, recorded_time):
     peaks, _ = find_peaks(red_values)
     pulse = (len(peaks) / float(recorded_time)) * 60
     pulse = np.int16(pulse)
-    print("pulse :" + str(pulse))
+    rospy.loginfo("[EulerianMotionMagnification] Pulse: " + str(pulse))
     return pulse, red_values
 
 
-def extract_red_values(gaussian_frame):
+def extract_red_values(gaussian_frame, show_processed_image):
     """
     Filters red-intensities of the image (Color channel 2) and adds to red_values list
     """
-    red_values = gaussian_frame[:, :, 2]
+    if show_processed_image:
+        red_values = []
+        for i in range(0, gaussian_frame.shape[0]):
+            red_value = gaussian_frame[i, :, 2]
+            red_values.append(red_value)
+    else:
+        red_values = gaussian_frame[:, :, 2]
     return red_values
+
+
+def upsample_images(processed_video, unprocessed_video, arraylength, levels):
+    """
+    Upsample images in video sequence to be able to display them.
+    Iterate through each image and add processed image to unprocessed image.
+    Upsample resulting image by reversing gaussian pyramide.
+    """
+    processed_video = np.asarray(processed_video, dtype=np.float32)
+    upsampled_images = np.zeros((arraylength, 200, 200, 3))
+    for i in range(0, processed_video.shape[0]):
+        img = processed_video[i]
+        img = img + unprocessed_video[i]
+        for x in range(levels):
+            img = cv2.pyrUp(img)
+        upsampled_images[i] = img
+    return upsampled_images
+
+
+def show_images(processed_video, unprocessed_video, arraylength, isFirst, levels, calculating_boarder, fps):
+    """
+    Show upsampled images to make color-intensity-changes visible to the human eye.
+    Show whole video-sequence if it's the first time executing. If it's not the first time,
+    show only new images.
+    :param processed_video: Array, containing processed images
+    :param unprocessed_video: Array, containing original frames without filtering and amplification
+    :param isFirst: indicates, whether first round of calculation
+    :param levels: depth of gaussian pyramide, indicates how many rounds are needed to upsample
+    :param calculating_boarder: how many images are new since last calculation
+    """
+    processed_video = upsample_images(processed_video, unprocessed_video, arraylength, levels)
+    if not isFirst:
+        processed_video = processed_video[-calculating_boarder:]
+    for image in processed_video:
+        time.sleep(1/fps)
+        cv2.imshow("colour changes pulse", image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
 class PulseMeasurement:
 
-    def __init__(self):
+    def __init__(self, show_processed_image):
+        self.show_processed_image = show_processed_image
         self.count = 0
         self.levels = 2
         self.low = 1.3
         self.high = 1.7
         self.amplification = 30
-        self.pub_pulse = rospy.Publisher('eulerian_color_changes_pulse', Float32, queue_size=10)
+        self.publisher = PulsePublisher("eulerian")
         self.fps = 30
         self.video_array = []
         self.buffer_size = 0
@@ -118,6 +164,8 @@ class PulseMeasurement:
         self.calculating_at = 0
         self.calculating_boarder = 50
         self.recording_time = 10
+        self.isFirst = True
+        self.arrayLength = 0
 
     def calculate_fps(self):
         """
@@ -129,17 +177,8 @@ class PulseMeasurement:
         if time_difference_in_seconds == 0:
             pass
         self.fps = self.buffer_size / time_difference_in_seconds
-        print("fps:" + str(self.fps))
-        print(len(self.video_array))
-
-    def publish_pulse(self, pulse, red_values):
-        """
-        Publish calculated pulse to ROS. Message is of type Float32.
-        :param pulse: calculated pulse value
-        """
-        msg_to_publish_pulse = pulse
-
-        self.pub_pulse.publish(msg_to_publish_pulse)
+        rospy.loginfo("[EulerianMotionMagnification] FPS: " + str(self.fps))
+        rospy.loginfo("[EulerianMotionMagnification] Video array length: " + str(len(self.video_array)))
 
     # calculate pulse after certain amount of images taken, calculation based on a larger amount of time
     def start_calulation(self, roi, timestamp):
@@ -149,8 +188,11 @@ class PulseMeasurement:
         normalized = cv2.normalize(roi.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
         cropped = cv2.resize(normalized, (200, 200))
         gaussian_frame = build_gaussian_frame(cropped, self.levels)
-        red_values_images = extract_red_values(gaussian_frame)
-        self.video_array.append(red_values_images)
+        if self.show_processed_image:
+            self.video_array.append(gaussian_frame)
+        else:
+            red_values_images = extract_red_values(gaussian_frame, self.show_processed_image)
+            self.video_array.append(red_values_images)
         # check if recording images took longer than certain amount of time
         time_difference = self.time_array[-1] - self.time_array[0]
         time_difference_in_seconds = time_difference.to_sec()
@@ -163,16 +205,21 @@ class PulseMeasurement:
             self.calculating_at = self.calculating_at + 1
             # calculate again after certain amount of images
             if self.calculating_at >= self.calculating_boarder:
-                print("length final " + str(len(self.video_array)))
+                rospy.loginfo("[EulerianMotionMagnification] Length final " + str(len(self.video_array)))
                 self.calculate_fps()
                 copy_video_array = np.copy(self.video_array)
                 copy_video_array = np.asarray(copy_video_array, dtype=np.float32)
                 copy_video_array = temporal_bandpass_filter(copy_video_array, self.low, self.high, self.fps)
-                copy_video_array = amplify_video(copy_video_array, amplify=self.amplification)
-                pulse, red_values = calculate_pulse(copy_video_array, self.recording_time)
-                self.publish_pulse(pulse, red_values)
+                if self.show_processed_image:
+                    self.arrayLength = len(self.video_array)
+                    processed_video = amplify_video(copy_video_array, amplify=self.amplification)
+                    show_images(processed_video, self.video_array, self.arrayLength, self.isFirst, self.levels, self.calculating_boarder, self.fps)
+                else:
+                    copy_video_array = amplify_video(copy_video_array, amplify=self.amplification)
+                pulse, red_values = calculate_pulse(copy_video_array, self.recording_time, self.show_processed_image)
+                self.publisher.publish(pulse, timestamp)
                 self.calculating_at = 0
-
+                self.isFirst = False
 
 
 def main():
@@ -180,30 +227,33 @@ def main():
 
     # Get ROS topic from launch parameter
     topic = rospy.get_param("~topic", "/webcam/image_raw")
-    rospy.loginfo("Listening on topic '" + topic + "'")
+    rospy.loginfo("[EulerianMotionMagnification] Listening on topic '" + topic + "'")
 
     video_file = rospy.get_param("~video_file", None)
-    rospy.loginfo("Video file input: '" + str(video_file) + "'")
+    rospy.loginfo("[EulerianMotionMagnification] Video file input: '" + str(video_file) + "'")
 
     bdf_file = rospy.get_param("~bdf_file", "")
-    rospy.loginfo("Bdf file: '" + str(bdf_file) + "'")
+    rospy.loginfo("[EulerianMotionMagnification] Bdf file: '" + str(bdf_file) + "'")
 
     cascade_file = rospy.get_param("~cascade_file", "")
-    rospy.loginfo("Cascade file: '" + str(cascade_file) + "'")
+    rospy.loginfo("[EulerianMotionMagnification] Cascade file: '" + str(cascade_file) + "'")
 
     show_image_frame = rospy.get_param("~show_image_frame", False)
-    rospy.loginfo("Show image frame: '" + str(show_image_frame) + "'")
+    rospy.loginfo("[EulerianMotionMagnification] Show image frame: '" + str(show_image_frame) + "'")
 
-    pulse_processor = PulseMeasurement()
+    show_processed_image = rospy.get_param("~show_processed_image", False)
+    rospy.loginfo("[EulerianMotionMagnification] Show processed frame: '" + str(show_processed_image) + "'")
+
+    pulse_processor = PulseMeasurement(show_processed_image)
 
     face_detector = FaceDetector(topic, cascade_file)
     face_detector.face_callback = pulse_processor.start_calulation
     face_detector.run(video_file, bdf_file, show_image_frame)
 
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
+    rospy.spin()
+    rospy.loginfo("[EulerianMotionMagnification] Shutting down")
+
+    # Destroy windows on close
     cv2.destroyAllWindows()
 
 
